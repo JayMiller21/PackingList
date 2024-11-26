ac3d776f403b0bc0d1e21ff7d551f97864f27bda
JayMiller21 <heyJayMiller21@gmail.com>
2024-11-26 12:37:05 -0500

    Add create-react-app client, and proxy for backend

--- NodeServer/app.js
+++ NodeServer/app.js
@@ -5,6 +5,6 @@ app.get('/', function(req, res) {
   res.send('Hello world!')
 })
 
-app.listen(3000, function() {
-  console.log('App listening on port 3000')
+app.listen(3001, function() {
+  console.log('App listening on port 3001')
 })
